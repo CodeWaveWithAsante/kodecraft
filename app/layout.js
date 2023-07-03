a57625e205ca187de422bdab9ece03820fae9b15
CodeWaveWithAsante <132752907+CodeWaveWithAsante@users.noreply.github.com>
2023-07-03 09:47:55 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "CodeCraft Solutions",
+  title: "KodeCraft Solutions",
   description: "A software development solution.",
 };
 
